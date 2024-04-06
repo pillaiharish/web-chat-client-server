71a8a7ff6582c0dc5cc24f905036757098c87979
harishkumar pillai <harishkumarpillai@harishkumars-Laptop.local>
2024-04-06 20:38:34 +0530

    Trivial echo server using websockets

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/pillaiharish/web-chat-client-server
 
 go 1.22.2
+
+require (
+	github.com/gorilla/websocket v1.5.1 // indirect
+	golang.org/x/net v0.24.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"io"
+	"net/http"
 
+	"golang.org/x/net/websocket"
+)
+
+// Echo the data received on the WebSocket.
+func EchoServer(ws *websocket.Conn) {
+	io.Copy(ws, ws)
+}
+
+// This example demonstrates a trivial echo server.
 func main() {
-    fmt.Println("Hello, World!")
+	http.Handle("/echo", websocket.Handler(EchoServer))
+	err := http.ListenAndServe(":12345", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
 }
 
